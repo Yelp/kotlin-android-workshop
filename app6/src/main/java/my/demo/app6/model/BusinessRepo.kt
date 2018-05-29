616ab0d7b40c7dccc9e2fb49508970f80851f328
Gesh Markov <gesh@yelp.com>
2018-05-29 18:04:09 +0200

    Made retrofit a bit shorter to use.

--- app6/src/main/java/my/demo/app6/model/BusinessRepo.kt
+++ app6/src/main/java/my/demo/app6/model/BusinessRepo.kt
@@ -1,14 +1,14 @@
 package my.demo.app6.model
 
 import io.reactivex.Single
-import my.demo.common.Apis
 import my.demo.common.Factory
 import my.demo.common.Provider
+import my.demo.common.api
 
 open class BusinessRepo {
     companion object : Factory<BusinessRepo> by Provider({ BusinessRepo() })
 
     open fun search(term: String): Single<BusinessSearchResponse> {
-        return Apis[BusinessSearch::class].search(term)
+        return api<BusinessSearch>().search(term)
     }
 }
--- common/src/main/java/my/demo/common/Apis.kt
+++ common/src/main/java/my/demo/common/Apis.kt
@@ -8,16 +8,14 @@ import okhttp3.OkHttpClient
 
 private const val AUTH = "Bearer NQISPs8iEEYN3gqoDkU5lU8ooRG23nSF9134tzyulLhBY6rbEWw87wnI9yg8UdR6QbFLGV17gsmlSS_OiimJWlAVv1Yu1fRvGabZirAYJNFnA3Joh0vmuyEyRU0EW3Yx"
 
+inline fun <reified T : Any> api(): T = Apis.create(T::class.java)
+
 object Apis {
     private val client = OkHttpClient.Builder().addNetworkInterceptor {
         it.proceed(it.request().newBuilder().addHeader("Authorization", AUTH).build())
     }.build()
 
-    operator fun <T : Any> get(service: KClass<T>): T {
-        return create(service.java)
-    }
-
-    private fun <T> create(service: Class<T>): T {
+    fun <T> create(service: Class<T>): T {
         return Retrofit.Builder()
                 .baseUrl("https://api.yelp.com/")
                 .client(client)
